e5d79180f2b94749a61d05c70af985ba28dcda71
ananya-2410 <ananya@hasura.io>
2022-07-01 14:47:44 +0530

    Handlers added

--- server.go
+++ server.go
@@ -1,56 +1,103 @@
 package main
 
 import (
-	"io/ioutil"
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/shaj13/go-guardian/auth"
+	"github.com/shaj13/go-guardian/auth/strategies/basic"
+	"github.com/shaj13/go-guardian/auth/strategies/bearer"
+	"github.com/shaj13/go-guardian/store"
 )
 
+var authenticator auth.Authenticator
+var cache store.Cache
+
+func getHasuraVariables(w http.ResponseWriter, r *http.Request) {
+	user, err := authenticator.Authenticate(r)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	hasuraVariables := map[string]string{
+		"X-Hasura-Role":    user.UserName(), // result.role
+		"X-Hasura-User-Id": user.ID(),
+	}
+
+	body := fmt.Sprintf("Hasura Variables: %s \n", hasuraVariables)
+
+	w.Write([]byte(body))
+}
+func get_user(username string) *auth.DefaultUser {
+	fmt.Println(username)
+	if username == "user1" {
+		return auth.NewDefaultUser("user1", "1", nil, nil)
+	} else if username == "user2" {
+		return auth.NewDefaultUser("user2", "2", nil, nil)
+	} else {
+		return auth.NewDefaultUser("anonymous", "0", nil, nil)
+	}
+}
+func createToken(w http.ResponseWriter, r *http.Request) {
+	token := uuid.New().String()
+	vars := mux.Vars(r)
+	user_name := vars["username"]
+	// user_id := r.URL.Query().Get("id")
+	// user_name := r.URL.Query().Get("username")
+	user := get_user(user_name)
+
+	tokenStrategy := authenticator.Strategy(bearer.CachedStrategyKey)
+	auth.Append(tokenStrategy, token, user, r)
+	body := fmt.Sprintf("token: %s \n", token)
+	w.Write([]byte(body))
+}
+
 func main() {
+	setupGoGuardian()
+	router := mux.NewRouter()
+	router.HandleFunc("/v1/login/{username}", middleware(http.HandlerFunc(createToken))).Methods("GET")
+	router.HandleFunc("/v1/verify", middleware(http.HandlerFunc(getHasuraVariables))).Methods("GET")
+	log.Println("Server started and listening on http://127.0.0.1:8080")
+	http.ListenAndServe("127.0.0.1:8081", router)
+}
 
-	client := http.Client{}
-	// Pass username password to auth0 to get token
-	req, err := http.NewRequest("GET", "/webhook/login", nil)
-	if err != nil {
-		log.Fatalln(err)
+func setupGoGuardian() {
+	authenticator = auth.New()
+	cache = store.NewFIFO(context.Background(), time.Minute*10)
+
+	basicStrategy := basic.New(validateUser, cache)
+	tokenStrategy := bearer.New(bearer.NoOpAuthenticate, cache)
+
+	authenticator.EnableStrategy(basic.StrategyKey, basicStrategy)
+	authenticator.EnableStrategy(bearer.CachedStrategyKey, tokenStrategy)
+}
+
+func validateUser(ctx context.Context, r *http.Request, userName, password string) (auth.Info, error) {
+	if userName == "user1" && password == "user1" {
+		return auth.NewDefaultUser("user1", "1", nil, nil), nil
+	}
+	if userName == "user2" && password == "user2" {
+		return auth.NewDefaultUser("user2", "2", nil, nil), nil
 	}
 
-	req.Header.Set("username", "abcd")
-	req.Header.Set("password", "pwd12345")
+	return nil, fmt.Errorf("Invalid credentials")
+}
 
-	resp, err := client.Do(req)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	// Get token
-	token := string(body)
-	// Use above token to verify
-	req1, err := http.NewRequest("GET", "/webhook/verify", nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	req1.Header.Set("Authorization", token)
-	req1.Header.Set("Content-Type", "application/json")
-	resp1, err := client.Do(req1)
-
-	if err != nil {
-		log.Fatalln(err)
-		log.Printf("X-Hasura-Role: Anonymous")
-	}
-	defer resp1.Body.Close()
-	// Get Role & User ID if token is valid
-	role := resp1.Header.Get("X-Hasura-Role")
-	user_id := resp1.Header.Get("X-Hasura-User-Id")
-
-	// Print role and user id
-	log.Printf(role)
-	log.Printf(user_id)
-
+func middleware(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing Auth Middleware")
+		user, err := authenticator.Authenticate(r)
+		if err != nil {
+			// 	code := http.StatusNotFound
+			fmt.Fprint(w, "ERROR 404 \t")
+			// 	http.Error(w, http.StatusText(code), code)
+			// 	return
+		}
+		log.Printf("User %s %s Authenticated\n", user.UserName(), user.ID())
+		next.ServeHTTP(w, r)
+	})
 }
