0670708a4d87f96dcf228463d2d0f89b015e64f7
ananya-2410 <ananya@hasura.io>
2022-07-04 17:54:13 +0530

    Working go file

--- server.go
+++ server.go
@@ -1,103 +1,78 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
-	"github.com/shaj13/go-guardian/auth"
-	"github.com/shaj13/go-guardian/auth/strategies/basic"
-	"github.com/shaj13/go-guardian/auth/strategies/bearer"
-	"github.com/shaj13/go-guardian/store"
 )
 
-var authenticator auth.Authenticator
-var cache store.Cache
+var (
+	token_map = make(map[string]string)
+)
+
+var creds_map = map[string]string{
+	"user1": "user1",
+	"user2": "user2",
+}
+
+var role_map = map[string]string{
+	"user1":     "1",
+	"user2":     "2",
+	"anonymous": "0",
+}
+
+type Foo struct {
+	Name string `json:"X-Hasura-Role"`
+	Id   string `json:"X-Hasura-User-Id"`
+}
 
 func getHasuraVariables(w http.ResponseWriter, r *http.Request) {
-	user, err := authenticator.Authenticate(r)
+	err := r.ParseForm()
 	if err != nil {
-		log.Fatalln(err)
+		http.Error(w, "Please pass the data as URL form encoded", http.StatusBadRequest)
+		return
 	}
-	hasuraVariables := map[string]string{
-		"X-Hasura-Role":    user.UserName(), // result.role
-		"X-Hasura-User-Id": user.ID(),
+	token := r.Form.Get("token")
+	username, ok := token_map[token]
+	if !ok {
+		username = "anonymous"
 	}
-
-	body := fmt.Sprintf("Hasura Variables: %s \n", hasuraVariables)
-
-	w.Write([]byte(body))
-}
-func get_user(username string) *auth.DefaultUser {
-	fmt.Println(username)
-	if username == "user1" {
-		return auth.NewDefaultUser("user1", "1", nil, nil)
-	} else if username == "user2" {
-		return auth.NewDefaultUser("user2", "2", nil, nil)
-	} else {
-		return auth.NewDefaultUser("anonymous", "0", nil, nil)
+	dict := map[string]string{
+		"X-Hasura-Role":    username,
+		"X-Hasura-User-Id": role_map[username],
 	}
-}
-func createToken(w http.ResponseWriter, r *http.Request) {
-	token := uuid.New().String()
-	vars := mux.Vars(r)
-	user_name := vars["username"]
-	// user_id := r.URL.Query().Get("id")
-	// user_name := r.URL.Query().Get("username")
-	user := get_user(user_name)
+	json_object, err := json.Marshal(dict)
 
-	tokenStrategy := authenticator.Strategy(bearer.CachedStrategyKey)
-	auth.Append(tokenStrategy, token, user, r)
-	body := fmt.Sprintf("token: %s \n", token)
-	w.Write([]byte(body))
+	log.Println(string(json_object))
 }
-
 func main() {
-	setupGoGuardian()
 	router := mux.NewRouter()
-	router.HandleFunc("/v1/login/{username}", middleware(http.HandlerFunc(createToken))).Methods("GET")
-	router.HandleFunc("/v1/verify", middleware(http.HandlerFunc(getHasuraVariables))).Methods("GET")
-	log.Println("Server started and listening on http://127.0.0.1:8080")
+
+	router.HandleFunc("/v1/login", createToken).Methods("POST")
+	router.HandleFunc("/v1/verify", getHasuraVariables).Methods("POST")
+	log.Println("Server started and listening on http://127.0.0.1:8000")
 	http.ListenAndServe("127.0.0.1:8081", router)
 }
 
-func setupGoGuardian() {
-	authenticator = auth.New()
-	cache = store.NewFIFO(context.Background(), time.Minute*10)
+func createToken(w http.ResponseWriter, r *http.Request) {
 
-	basicStrategy := basic.New(validateUser, cache)
-	tokenStrategy := bearer.New(bearer.NoOpAuthenticate, cache)
-
-	authenticator.EnableStrategy(basic.StrategyKey, basicStrategy)
-	authenticator.EnableStrategy(bearer.CachedStrategyKey, tokenStrategy)
-}
-
-func validateUser(ctx context.Context, r *http.Request, userName, password string) (auth.Info, error) {
-	if userName == "user1" && password == "user1" {
-		return auth.NewDefaultUser("user1", "1", nil, nil), nil
+	token := uuid.New().String()
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Please pass the data as URL form encoded", http.StatusBadRequest)
+		return
 	}
-	if userName == "user2" && password == "user2" {
-		return auth.NewDefaultUser("user2", "2", nil, nil), nil
+	username := r.Form.Get("username")
+	pwd := r.Form.Get("password")
+	if creds_map[username] != pwd {
+		fmt.Println("Error credentials")
+		log.Fatalln("ERROR CREDENTIALS")
 	}
-
-	return nil, fmt.Errorf("Invalid credentials")
-}
-
-func middleware(next http.Handler) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Executing Auth Middleware")
-		user, err := authenticator.Authenticate(r)
-		if err != nil {
-			// 	code := http.StatusNotFound
-			fmt.Fprint(w, "ERROR 404 \t")
-			// 	http.Error(w, http.StatusText(code), code)
-			// 	return
-		}
-		log.Printf("User %s %s Authenticated\n", user.UserName(), user.ID())
-		next.ServeHTTP(w, r)
-	})
+	body := fmt.Sprintf("token: %s \n", token)
+	token_map[token] = username
+	w.Write([]byte(body))
 }
